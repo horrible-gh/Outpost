f50bd0fb146614d43819539f26f5663fe336d5fc
horrible-gh <132524887+horrible-gh@users.noreply.github.com>
2026-09-08 07:09:11 +0900

    fix journal report type

--- internal/patrol/journal.go
+++ internal/patrol/journal.go
@@ -15,38 +15,53 @@ type Journal struct {
 
 func NewJournal(path string) *Journal { return &Journal{path: path} }
 
-func (j *Journal) Append(report Report) error {
+func (j *Journal) Append(report PatrolReport) error {
 	j.mu.Lock()
 	defer j.mu.Unlock()
 	f, err := os.OpenFile(j.path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o600)
-	if err != nil { return err }
+	if err != nil {
+		return err
+	}
 	defer f.Close()
 	return json.NewEncoder(f).Encode(report)
 }
 
-func (j *Journal) Recent(limit int) ([]Report, error) {
+func (j *Journal) Recent(limit int) ([]PatrolReport, error) {
 	j.mu.Lock()
 	defer j.mu.Unlock()
 	f, err := os.Open(j.path)
-	if errors.Is(err, os.ErrNotExist) { return []Report{}, nil }
-	if err != nil { return nil, err }
+	if errors.Is(err, os.ErrNotExist) {
+		return []PatrolReport{}, nil
+	}
+	if err != nil {
+		return nil, err
+	}
 	defer f.Close()
 
-	var all []Report
+	var all []PatrolReport
 	s := bufio.NewScanner(f)
 	buf := make([]byte, 64*1024)
 	s.Buffer(buf, 4*1024*1024)
 	for s.Scan() {
-		var r Report
-		if json.Unmarshal(s.Bytes(), &r) == nil { all = append(all, r) }
+		var r PatrolReport
+		if json.Unmarshal(s.Bytes(), &r) == nil {
+			all = append(all, r)
+		}
 	}
-	if err := s.Err(); err != nil { return nil, err }
-	if limit <= 0 || len(all) <= limit { reverse(all); return all, nil }
+	if err := s.Err(); err != nil {
+		return nil, err
+	}
+	if limit <= 0 || len(all) <= limit {
+		reverse(all)
+		return all, nil
+	}
 	all = all[len(all)-limit:]
 	reverse(all)
 	return all, nil
 }
 
-func reverse(v []Report) {
-	for i, k := 0, len(v)-1; i < k; i, k = i+1, k-1 { v[i], v[k] = v[k], v[i] }
+func reverse(v []PatrolReport) {
+	for i, k := 0, len(v)-1; i < k; i, k = i+1, k-1 {
+		v[i], v[k] = v[k], v[i]
+	}
 }
